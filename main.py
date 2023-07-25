ab478488bbc4f7d4272647c47dc35e53dca24f75
tandavbot <140485849+tandavbot@users.noreply.github.com>
2023-07-26 01:42:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+from pyromod import listen
+from pyrogram import Client, filters
+
+
+Bot = Client(
+    "Instant-Caption-Adder",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+CAPTION = os.environ.get("CAPTION", None)
+
+# Better to add caption through config vars / app.json
+
+
+@Bot.on_message(filters.media)
+async def caption(bot, message):
+    chat_id = message.chat.id
+    if CAPTION:
+        caption = CAPTION
+    else:
+        caption = await get_caption(bot, message)
+        if caption is True:
+            return
+        await message.copy(chat_id=chat_id, caption=caption, reply_to_message_id=message.message_id)
+
+
+async def get_caption(bot, message):
+    caption = await bot.ask(message.chat.id, "Send a caption for the media or send /cancel for cancelling this process")
+    if not caption.text:
+        await caption.reply("No caption found", quote=True)
+        return await get_caption(bot, message)
+    if caption.text.startswith("/cancel"):
+        await caption.reply("Process cancelled", quote=True)
+        return True
+    else:
+        return caption.text
+
+
+Bot.run()
